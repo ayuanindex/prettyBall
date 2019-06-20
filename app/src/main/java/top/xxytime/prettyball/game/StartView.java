a4b8df31b0ca6cc2aca62b850c717f0e273646d1
ayuan <lzy2118053626@hotmail.com>
2019-06-20 15:43:13 +0800

    update

--- app/src/main/java/top/xxytime/prettyball/game/StartView.java
+++ app/src/main/java/top/xxytime/prettyball/game/StartView.java
@@ -1,9 +1,13 @@
 package top.xxytime.prettyball.game;
 
+import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 
 import java.util.Timer;
+
+import top.xxytime.prettyball.main.Main;
+import top.xxytime.prettyball.utils.Tools;
 
 /**
  * 开始界面，点击开始界面经过家在过程到达主界面
@@ -48,4 +52,29 @@ public class StartView {
      * 下标：Loading动画的下标
      */
     private int niFrame;
+
+    /**
+     * 计数：按下任意键
+     */
+    private int niPressAnythingCount;
+
+    /**
+     * 状态：按任意键开始
+     */
+    private boolean isPressAnything;
+
+    /**
+     * 在构造方法中进行赋值
+     */
+    public StartView() {
+        bmpBackground = Tools.readBitmapFromAssets("image/system/start.png");
+
+        pBackground = new Point(
+                Main.getRECT_GANESCREEN_X() + (Main.getRECT_GANESCREEN_WIDTH() - bmpBackground.getWidth()) / 2,
+                Main.getRECT_GANESCREEN_Y() + (Main.getRECT_GANESCREEN_HEIGHT() - bmpBackground.getHeight()) / 2
+        );
+
+        pLoading = Tools.readBitmapFolderFromAssets("image/system/loading");
+
+    }
 }
