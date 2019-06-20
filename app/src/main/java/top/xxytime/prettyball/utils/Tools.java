379894cfb186ad1049e09309c2028cac4129d32a
ayuan <lzy2118053626@hotmail.com>
2019-06-20 14:49:35 +0800

    update

--- app/src/main/java/top/xxytime/prettyball/utils/Tools.java
+++ app/src/main/java/top/xxytime/prettyball/utils/Tools.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import top.xxytime.prettyball.main.Info;
 import top.xxytime.prettyball.main.Main;
@@ -32,12 +33,38 @@ public final class Tools {
         return bmp;
     }
 
-    // 打印错误
+    /**
+     * 获取assets中指定文件夹中的所有资源图片
+     *
+     * @param strDir 文件夹名称
+     * @return 返回资源图片数组
+     */
+    public static final Bitmap[] readBitmapFolderFromAssets(String strDir) {
+        try {
+            // 获取指定文件夹下的资源图片所有的资源图片名称
+            String[] arrStrFileName = null;
+            arrStrFileName = Main.getAsset().list(strDir);
+            Tools.LogInfo(Arrays.toString(arrStrFileName));
+        } catch (IOException e) {
+            Tools.LogError(strDir + "error------->" + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 打印错误
+     *
+     * @param strMsg
+     */
     private static void LogError(String strMsg) {
         Log.e(Info.STR_LOG_TAG, strMsg);
     }
 
-    // 打印调试信息
+    /**
+     * 打印调试信息
+     *
+     * @param strMsg
+     */
     public static void LogInfo(String strMsg) {
         Log.e(Info.STR_LOG_TAG, strMsg);
     }
